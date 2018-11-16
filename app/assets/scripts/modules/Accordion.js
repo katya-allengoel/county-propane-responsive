6ea2c53449c5b453285f79c42b4f3853a89159b6
katya-allengoel <katya@allengoel.com>
2018-11-16 12:34:59 -0500

    accordion working

--- app/assets/scripts/modules/Accordion.js
+++ app/assets/scripts/modules/Accordion.js
@@ -1,3 +1,27 @@
+function openFirstPanel(){
+  $('.accordion > .accordion__title:first-child').next().addClass('active').slideDown();
+}
 
+(function($) {
+    
+  var allPanels = $('.accordion > .accordion__description').hide();
+  
+  openFirstPanel();
+    
+  $('.accordion > .accordion__title > a').click(function(e) {
+  	e.preventDefault();
+  	console.log($(this).text());
+      var $target =  $(this).parent().next();
+      
+    
+      if($target.hasClass('active')){
+        $target.removeClass('active').slideUp(); 
+      }else{
+        allPanels.removeClass('active').slideUp();
+        $target.addClass('active').slideDown();
+      }
+      
+    return false;
+  });
 
-$('.accordion').accordion();
+})(jQuery);
--- app/temp/scripts/App.js
+++ app/temp/scripts/App.js
@@ -14543,10 +14543,34 @@ return Flickity;
 /***/ (function(module, exports, __webpack_require__) {
 
 "use strict";
-/* WEBPACK VAR INJECTION */(function($) {
+/* WEBPACK VAR INJECTION */(function($, jQuery) {
 
-$('.accordion').accordion();
-/* WEBPACK VAR INJECTION */}.call(exports, __webpack_require__(2)))
+function openFirstPanel() {
+  $('.accordion > .accordion__title:first-child').next().addClass('active').slideDown();
+}
+
+(function ($) {
+
+  var allPanels = $('.accordion > .accordion__description').hide();
+
+  openFirstPanel();
+
+  $('.accordion > .accordion__title > a').click(function (e) {
+    e.preventDefault();
+    console.log($(this).text());
+    var $target = $(this).parent().next();
+
+    if ($target.hasClass('active')) {
+      $target.removeClass('active').slideUp();
+    } else {
+      allPanels.removeClass('active').slideUp();
+      $target.addClass('active').slideDown();
+    }
+
+    return false;
+  });
+})(jQuery);
+/* WEBPACK VAR INJECTION */}.call(exports, __webpack_require__(2), __webpack_require__(2)))
 
 /***/ }),
 /* 24 */
